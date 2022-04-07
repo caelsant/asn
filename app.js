cf3a8b915b1f9b3e5a531991d2e77f74858e2e92
Micael Santana <51369599+caelsant@users.noreply.github.com>
2022-04-06 22:21:06 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+var express = require("express");
+var app = express();
+const path = require("path")
+
+app.use(express.static('public'))
+app.use(express.static('css'))
+app.use(express.static('media'))
+
+app.get("/index", function(req, res){
+    res.sendFile(path.join(__dirname + "/index.html"))
+})
+
+app.get("/dashboard", function(req, res){
+    res.sendFile(path.join(__dirname + "/dashboard.html"))
+})
+
+
+
+app.listen(9090, function(){
+    console.log("Rodando Normalmente")
+});
