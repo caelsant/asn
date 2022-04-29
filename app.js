95931a2a3b12769f0fbe26b173986a597fd5a493
Micael Santana <51369599+caelsant@users.noreply.github.com>
2022-04-29 08:57:45 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const path = require("path");
     app.set('view engine', 'handlebars');
 
    // app.use(bodyParser.urlencoded({extended: false}))
-    app.use(bodyParser.json())
+    //app.use(bodyParser.json())
 
     app.use(express.static('public'));
 
