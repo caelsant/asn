6ff8a8abba95a145495bc40f482af5f0cbb0b84c
Cael <micael.santana233@gmail.com>
2022-05-02 15:28:08 -0300

    Editei os mobile

--- models/db.js
+++ models/db.js
@@ -1,12 +1,14 @@
 const Sequelize = require('sequelize');
 
-const db = new Sequelize( 
-    "ejsdb",
-   "admin",
-   "66325489", {
-    host: "ejs-db-2.c1c2ntdfepv4.sa-east-1.rds.amazonaws.com",
-    dialect: "mysql"
-});
+const db = new Sequelize(
+    "asn",
+    "root",
+    "6632",
+    {
+        host: "localhost",
+        dialect: "mysql"
+    }
+);
 
 db.authenticate().then(
     function(){
